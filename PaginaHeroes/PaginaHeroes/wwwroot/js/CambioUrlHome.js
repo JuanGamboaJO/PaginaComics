b0faf3db570591810a65c59d95c47a18e82a62bb
JuanGamboaJO <86616609+JuanGamboaJO@users.noreply.github.com>
2021-07-14 16:16:47 -0500

    CAMBIO DE ID

---
+++ PaginaHeroes/PaginaHeroes/wwwroot/js/CambioUrlHome.js
@@ -0,0 +1,12 @@
+﻿window.addEventListener("load", function () {
+
+    var codigo = Math.random().toString(36).substr(2);
+
+    sessionStorage.setItem("Homeurl", codigo);
+
+    data = sessionStorage.getItem("Homeurl");
+
+    console.log(data);
+
+    window.location.replace(`?Home=${data}`);
+})
